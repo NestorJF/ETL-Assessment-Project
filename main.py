18a2b9e9dc11cea678d79f5739cf7906966981b1
Néstor Flores <nesjfgon@gmail.com>
2023-02-04 15:13:04 -0600

    - Add car sales dataset. - Add selection of datasource

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-
+from src.extract import Extract
 
 
 if __name__ == "__main__":
+    extract = Extract("data/input/car_sales_dataset.csv")
+    dataset = extract.extract_data("csv")
     print("Hi")
+    print(dataset)
--- src/extract.py
+++ src/extract.py
@@ -4,7 +4,15 @@ class Extract:
     def __init__(self, file_path):
         self.file_path = file_path
         
-    def extract_from_csv(self):
+    def extract_data(self, datasource: str = "csv"):
+        dataset = pd.DataFrame()
+        if datasource == "csv":
+            dataset = self._extract_from_csv()
+        else:
+            print("No datasource")
+        return dataset
+        
+    def _extract_from_csv(self):
         """
         Reads the CSV file specified in the constructor and returns a Pandas DataFrame
         :return: DataFrame containing the extracted data            
