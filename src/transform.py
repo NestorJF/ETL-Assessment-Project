88a41230da2c347caf2ec8821be3aa80806bee21
Néstor Flores <nesjfgon@gmail.com>
2023-02-04 16:16:47 -0600

    Start Transformation process: Remove any rows with missing values.

---
+++ src/transform.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+class Transformation:
+    def __init__(self, datasource, dataset):
+        self.datasource = datasource
+        self.dataset = dataset
+
+    def start_transformation(self):
+        if self.datasource == "csv":
+            new_dataset = self._transform_dataframe()
+        else:
+            print("Datasource not found. Transformation failed.")
+            new_dataset = self.dataset
+        return new_dataset
+
+    def _transform_dataframe(self):
+        new_dataset = self.dataset.copy()
+        new_dataset = new_dataset.dropna()
+        return new_dataset
